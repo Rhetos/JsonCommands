58e17f08af8afa5a655fc92900062f5159376855
kfranic1 <karlofranic0@gmail.com>
2024-04-25 11:57:33 +0200

    `Command` parsing should fail if there are multiple same operations

--- src/Rhetos.JsonCommands.Host/Parsers/Write/Command.cs
+++ src/Rhetos.JsonCommands.Host/Parsers/Write/Command.cs
@@ -8,20 +8,28 @@ namespace Rhetos.JsonCommands.Host.Parsers.Write
     public class Command
     {
         public string Entity { get; set; }
-        public List<SaveOperationItems> Operations { get; set; }
+        public List<SaveOperationItems> Operations {
+            get => _operations;
+            set
+            {
+                if (value.Where((op) => op.IsDelete).Count() > 1) throw new FormatException("There are multiple Delete elements. Only one is allowed");
+                if (value.Where((op) => op.IsInsert).Count() > 1) throw new FormatException("There are multiple Insert elements. Only one is allowed");
+                if (value.Where((op) => op.IsUpdate).Count() > 1) throw new FormatException("There are multiple Update elements. Only one is allowed");
+                _operations = value;
+            }
+        }
+
+        private List<SaveOperationItems> _operations;
         public IEntity[] DeleteOperationItems()
         {
-            if (Operations.Where((op) => op.IsDelete).Count() > 1) throw new FormatException("There are multiple Delete elements. Only one is allowed");
             return Operations.FirstOrDefault((op) => op.IsDelete)?.Items;
         }
         public IEntity[] InsertOperationItems()
         {
-            if (Operations.Where((op) => op.IsInsert).Count() > 1) throw new FormatException("There are multiple Insert elements. Only one is allowed");
             return Operations.FirstOrDefault((op) => op.IsInsert)?.Items;
         }
         public IEntity[] UpdateOperationItems()
         {
-            if (Operations.Where((op) => op.IsUpdate).Count() > 1) throw new FormatException("There are multiple Update elements. Only one is allowed");
             return Operations.FirstOrDefault((op) => op.IsUpdate)?.Items;
         }
     }
